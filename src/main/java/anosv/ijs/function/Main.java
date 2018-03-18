56f009ec4219883e556c02dc9b99e086a11bb7ac
anosv <crazzyzyzy@rambler.ru>
2018-03-18 21:31:24 +0300

    add lambda

---
+++ src/main/java/anosv/ijs/function/Main.java
@@ -0,0 +1,27 @@
+package anosv.ijs.function;
+
+public class Main {
+
+    public static void main(String[] args) {
+        doAction(() ->
+                System.out.println("FFFFFUUUUUUUUUUUNNNNNNNN!!!!!!!!!"));
+
+        doAction(new Fun() {
+            @Override
+            public void go() {
+                System.out.println("No Lambda!");
+            }
+        });
+
+    }
+
+    interface Fun {
+        void go();
+    }
+
+    public static void doAction(Fun fun) {
+        System.out.println("Start!");
+        fun.go();
+        System.out.println("End!");
+    }
+}
