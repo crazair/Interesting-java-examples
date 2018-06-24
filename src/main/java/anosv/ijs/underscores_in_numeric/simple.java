4c884bd68b1ff2e73d6e2cde42ab33e98b04f1de
anosv <crazzyzyzy@rambler.ru>
2018-06-24 21:01:29 +0300

    ADD num!

---
+++ src/main/java/anosv/ijs/underscores_in_numeric/simple.java
@@ -0,0 +1,15 @@
+package anosv.ijs.underscores_in_numeric;
+
+public class simple {
+
+    public static void main(String[] args) {
+
+        int maxUploadSize   = 20_971_520;
+        long accountBalance = 1_000_000_000_000L;
+        float pi            = 3.141_592_653_598F;
+
+        System.out.println(maxUploadSize);
+        System.out.println(accountBalance);
+        System.out.println(pi);
+    }
+}
